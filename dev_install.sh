38722a4565b9ce44b887bb5fb9b15f4f2a1cf0f6
Brandon Istenes <bistenes@gmail.com>
2020-02-04 16:29:25 -0500

    Add script to update dependencies to github master for development

---
+++ dev_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+npm install --no-save \
+    git+ssh://git@github.com:openmrs/openmrs-esm-api.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-devtools.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-error-handling.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-home.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-login.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-module-config.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-primary-navigation.git \
+    git+ssh://git@github.com:openmrs/openmrs-esm-styleguide.git \
+    git+ssh://git@github.com:pih/pih-esm-root-config.git
